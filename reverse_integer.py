2d34aa10ec3ec8fd272811fd0f033767ab93b50c
GeorgeCloud <net.george.sky@gmail.com>
2022-03-30 09:54:36 -0700

    2 Q's

---
+++ reverse_integer.py
@@ -0,0 +1,27 @@
+class Solution(object):
+    def reverse(self, x):
+        """
+        :type x: int
+        :rtype: int
+        """
+        if x == 0:
+            # escape early due to edge case
+            return 0
+        elif x > 0:
+            # safe to reverse string with built-in slice method
+            cand = int(str(x)[::-1])
+            # ensure candidate is not out of bound
+            if -2**31 <= cand <= 2**31 - 1:
+                return cand
+            else:
+                # if out of bound return 0
+                return 0
+        else:
+            # return negative number into positive then reverse then turn number back to negative
+            cand = -int(str(-x)[::-1])
+            # ensure candidate is not out of bound
+            if -2**31 <= cand <= 2**31 - 1:
+                return cand
+            else:
+                # if out of bound return 0
+                return 0
---
+++ valid_parentheses.py
@@ -0,0 +1,15 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        pair = dict(('()', '[]', '{}'))
+        stack = []
+        for c in s:
+            # if current character is an opening parentheses
+            if c in '([{':
+                # treat list as a stack and remove the first item
+                stack.append(c)
+            # if current char is a closing bracket and stack is empty then return False
+            elif len(stack) == 0 or c != pair[stack.pop()]:
+                return False
+
+        # if stack is empty then true else false
+        return len(stack) == 0
